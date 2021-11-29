2fa0383f3c760fd335286603c88be5d8856912f0
Usman Malik <usmanmaliksahib2611@gmail.com>
2021-11-30 01:51:40 +0500

    domotica

--- app/src/main/java/com/example/domotica/Register.java
+++ app/src/main/java/com/example/domotica/Register.java
@@ -6,22 +6,109 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Register extends AppCompatActivity {
 
-    TextView tv;
+    TextInputLayout fullname_var, username_var, email_var, phonenumber_var, password_var;
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference reference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        tv=(TextView) findViewById(R.id.already_have_account);
-        tv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(getApplicationContext(),login.class);
-                startActivity(intent);
+
+        fullname_var = findViewById(R.id.full_name);
+        username_var = findViewById(R.id.user_name);
+        email_var = findViewById(R.id.email);
+        phonenumber_var = findViewById(R.id.phone_number);
+        password_var = findViewById(R.id.password_register);
+
+    }
+
+    public void loginbuttonclick(View view) {
+        Intent intent = new Intent(getApplicationContext(),login.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void registerbuttonclick(View view) {
+
+        String fullname_ =fullname_var.getEditText().getText().toString();
+        String username_ =username_var.getEditText().getText().toString();
+        String email_ =email_var.getEditText().getText().toString();
+        String phonenumber_ =phonenumber_var.getEditText().getText().toString();
+        String password_ =password_var.getEditText().getText().toString();
+
+        if(!fullname_.isEmpty()){
+            fullname_var.setError(null);
+            fullname_var.setErrorEnabled(false);
+            if (!username_.isEmpty()){
+                username_var.setError(null);
+                username_var.setErrorEnabled(false);
+                if (!email_.isEmpty()){
+                    email_var.setError(null);
+                    email_var.setErrorEnabled(false);
+                    if (!phonenumber_.isEmpty()){
+                        phonenumber_var.setError(null);
+                        phonenumber_var.setErrorEnabled(false);
+                        if (!password_.isEmpty()){
+                            password_var.setError(null);
+                            password_var.setErrorEnabled(false);
+
+                            firebaseDatabase = FirebaseDatabase.getInstance();
+                            reference = firebaseDatabase.getReference();
+
+                            //Storing data
+
+                            String fullname_s =fullname_var.getEditText().getText().toString();
+                            String username_s =username_var.getEditText().getText().toString();
+                            String email_s =email_var.getEditText().getText().toString();
+                            String phonenumber_s =phonenumber_var.getEditText().getText().toString();
+                            String password_s =password_var.getEditText().getText().toString();
+
+                            storingdata storingdatass = new storingdata(fullname_s,username_s,email_s,phonenumber_s,password_s);
+
+                            reference.child(username_s).setValue(storingdatass);
+
+                            Toast.makeText(getApplicationContext(),"Register Successfully",Toast.LENGTH_SHORT).show();
+                             Intent intent = new Intent(getApplicationContext(),login.class);
+                             startActivity(intent);
+                             finish();
+
+
+
+
+
+                        }
+                        else{
+                            password_var.setError("Please Enter Password");
+                        }
+
+                    }
+                    else{
+                        phonenumber_var.setError("Please Enter PhoneNumber");
+                    }
+
+                }
+                else{
+                    email_var.setError("Please Enter Email");
+                }
 
             }
-        });
+            else{
+                username_var.setError("Please Enter UserName");
+            }
+
+        }
+        else{
+            fullname_var.setError("Please Enter FullName");
+        }
     }
 }
--- app/src/main/java/com/example/domotica/login.java
+++ app/src/main/java/com/example/domotica/login.java
@@ -1,24 +1,118 @@
 package com.example.domotica;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class login extends AppCompatActivity {
 
     TextView tv;
-    Button btn1;
+    Button loginbtn;
+    TextInputLayout username_var,password_var;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.login);
+
         tv=findViewById(R.id.login_btn);
-        btn1=findViewById(R.id.logbtn);
-        btn1.setOnClickListener(new View.OnClickListener() {
+        loginbtn=findViewById(R.id.logbtn);
+
+        //Assign variable to input text field of login screen
+        username_var = findViewById(R.id.username_text_field_design);
+        password_var = findViewById(R.id.password_input_field);
+
+
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String username_ =username_var.getEditText().getText().toString();
+                String password_ =password_var.getEditText().getText().toString();
+
+                if (!username_.isEmpty()){
+                    username_var.setError(null);
+                    username_var.setErrorEnabled(false);
+                    if (!password_.isEmpty()){
+                        username_var.setError(null);
+                        password_var.setErrorEnabled(false);
+
+                        final String username_data = username_var.getEditText().getText().toString();
+                        final String password_data = password_var.getEditText().getText().toString();
+
+                        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+                        DatabaseReference databaseReference = firebaseDatabase.getReference("userdata");
+
+                        Query check_username = databaseReference.orderByChild("username").equalTo(username_data);
+
+                        check_username.addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                                if (snapshot.exists()){
+                                    username_var.setError(null);
+                                    username_var.setErrorEnabled(false);
+                                    String passwordcheck = snapshot.child(username_data).child("password").getValue(String.class);
+                                    if (passwordcheck.equals(password_data)){
+                                        password_var.setError(null);
+                                        password_var.setErrorEnabled(false);
+                                        Toast.makeText(getApplicationContext(),"Login Successfully",Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(getApplicationContext(),login.class);
+                                        startActivity(intent);
+                                        finish();
+
+                                    }
+                                    else {
+                                        password_var.setError("Wrong Password");
+                                    }
+
+                                }
+                                else {
+                                    username_var.setError("User does not exists");
+                                }
+
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        });
+
+
+
+                    }
+                    else {
+                        password_var.setError("Please Enter the password");
+                    }
+
+                }
+                else {
+                    username_var.setError("Please Enter the Username");
+                }
+
+
+            }
+        });
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(getApplicationContext(),home.class);
@@ -26,6 +120,7 @@ public class login extends AppCompatActivity {
 
             }
         });
+
         tv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,5 +130,8 @@ public class login extends AppCompatActivity {
             }
         });
 
+
     }
+
+
 }
