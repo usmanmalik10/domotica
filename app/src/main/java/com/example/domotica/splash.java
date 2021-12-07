a823b184f5d791341f7b7cb4d9f3078370bee0b3
Usman Malik <usmanmaliksahib2611@gmail.com>
2021-12-07 12:14:46 +0500

    splash

--- app/src/main/java/com/example/domotica/splash.java
+++ app/src/main/java/com/example/domotica/splash.java
@@ -1,14 +1,28 @@
 package com.example.domotica;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
-
 public class splash extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Thread t=new Thread()
+        {
+            public void run()
+            {
+                try {
+                    sleep(1000*3);
+                    finish();
+                    Intent cv=new Intent(splash.this,login.class);
+                    startActivity(cv);
+                }
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        t.start();
     }
 }
+
